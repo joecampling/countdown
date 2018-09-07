0a6fb87d4b73890b5e963026914660abba3a7126
Joseph Campling <j.campling@soton.ac.uk>
2018-09-07 22:28:52 +0100

    Rewrote functions to produce massive speed-up

--- countdown.py
+++ countdown.py
@@ -7,27 +7,36 @@ Created on Thu Sep  6 15:18:20 2018
 import pandas as pd
 import numpy as np
 
-from functions import adder, multiplier, division, minus
+from functions import adder, multiplier, division, minus, adder_arr, minus_arr, multiplier_arr, divider_arr
 
-
-print('err, hellooo?....')
 df = pd.DataFrame({'Numbers': [], 'History': []})
-numbers = [1, 2, 3, 4, 5, 6]
+numbers = [[1, 2, 3, 4, 5, 6]]
+history = [[]]
 target = 125
-df = df.append({'Numbers': np.sort(numbers), 'History': []}, ignore_index = True)
+df = df.append({'Numbers': numbers, 'History': []}, ignore_index = True)
 print('Numbers:', numbers)
 print('Target:', target)
 processing = True
 i = 0
+
 while processing:
     try:
-        df.iloc[i]
+        numbers[i]
     except:
-        processing = False
         break
+    numbers, history = adder_arr(numbers[i], history[i], numbers, history, target)
+    numbers, history = multiplier_arr(numbers[i], history[i], numbers, history, target)
+    numbers, history = minus_arr(numbers[i], history[i], numbers, history, target)
+    numbers, history = divider_arr(numbers[i], history[i], numbers, history, target)
+    i += 1
+    
+'''    
+
+while processing:
+
     df = adder(df['Numbers'].iloc[i], df['History'].iloc[i], df, target)
     df = multiplier(df['Numbers'].iloc[i], df['History'].iloc[i], df, target)
     df = minus(df['Numbers'].iloc[i], df['History'].iloc[i], df, target)
     df = division(df['Numbers'].iloc[i], df['History'].iloc[i], df, target)
     i += 1
-    
+'''
--- functions.py
+++ functions.py
@@ -40,24 +40,111 @@ class divider_object:
                         
 def adder(array, history, df, target):
     l = len(array)
+    temparr = []
+    arr_of_dfs = []
+    arr_of_dfs.append(df)
     for a in range(l):
         for b in np.arange(a + 1, l):
-            answer = array[a] + array[b]
             temphist = history.copy()
-            temphist.append('{} + {} = {}'.format(array[a], array[b], answer))
-            if answer == target:
+            temphist.append('{} + {} = {}'.format(array[a], array[b], array[a] + array[b]))
+            if array[a] + array[b] == target:
                     print(temphist)
-            temparr = np.copy(array)
+            temparr = array.copy()
             temparr[a] += temparr[b]
-            np.delete(temparr, b)
-            '''
-            temp = pd.DataFrame({'Numbers': [], 'History': []})
-            temp = temp.append({'Numbers': temparr, 'History': temphist}, ignore_index = True)
-            '''
-            df = df.append({'Numbers': temparr, 'History': temphist}, ignore_index = True)
-    return df
+            temparr.remove(temparr[b])
+            tempdf = pd.DataFrame({'Numbers': [temparr], 'History': [temphist]})
+            arr_of_dfs.append(tempdf)
+    
+    return pd.concat(arr_of_dfs)
+
+def adder_arr(numbers, history, numarr, histarr, target):
+    l = len(numbers)
+    for a in range(l):
+        for b in np.arange(a + 1, l):
+            temphist = history.copy()
+            temphist.append('{} + {} = {}'.format(numbers[a], numbers[b], numbers[a] + numbers[b]))
+            if numbers[a] + numbers[b] == target:
+                    print(temphist)
+            temparr = numbers.copy()
+            temparr[a] += temparr[b]
+            temparr.remove(temparr[b])
+            numarr.append(temparr.copy())
+            histarr.append(temphist.copy())
+    
+    return numarr, histarr
+
+def multiplier_arr(numbers, history, numarr, histarr, target):
+    l = len(numbers)
+    for a in range(l):
+        for b in np.arange(a + 1, l):
+            temphist = history.copy()
+            temphist.append('{} * {} = {}'.format(numbers[a], numbers[b], numbers[a] * numbers[b]))
+            if numbers[a] * numbers[b] == target:
+                    print(temphist)
+            temparr = numbers.copy()
+            temparr[a] *= temparr[b]
+            temparr.remove(temparr[b])
+            numarr.append(temparr.copy())
+            histarr.append(temphist.copy())
+    
+    return numarr, histarr
+
+def minus_arr(numbers, history, numarr, histarr, target):
+    numbers.sort(reverse = True)
+    l = len(numbers)
+    for a in range(l):
+        for b in np.arange(a + 1, l):
+            if numbers[a] > numbers[b]:
+                temphist = history.copy()
+                temphist.append('{} - {} = {}'.format(numbers[a], numbers[b], numbers[a] - numbers[b]))
+                if numbers[a] - numbers[b] == target:
+                        print(temphist)
+                temparr = numbers.copy()
+                temparr[a] -= temparr[b]
+                temparr.remove(temparr[b])
+                numarr.append(temparr.copy())
+                histarr.append(temphist.copy())
+    
+    return numarr, histarr
+
+def divider_arr(numbers, history, numarr, histarr, target):
+    numbers.sort(reverse = True)
+    l = len(numbers)
+    for a in range(l):
+        for b in np.arange(a + 1, l):
+            if numbers[a] % numbers[b] == 0:
+                temphist = history.copy()
+                temphist.append('{} / {} = {}'.format(numbers[a], numbers[b], numbers[a] / numbers[b]))
+                if numbers[a] / numbers[b] == target:
+                        print(temphist)
+                temparr = numbers.copy()
+                temparr[a] /= temparr[b]
+                temparr.remove(temparr[b])
+                numarr.append(temparr.copy())
+                histarr.append(temphist.copy())
+    
+    return numarr, histarr
 
 def multiplier(array, history, df, target):
+    l = len(array)
+    temparr = []
+    arr_of_dfs = []
+    arr_of_dfs.append(df)
+    for a in range(l):
+        for b in np.arange(a + 1, l):
+            temphist = history.copy()
+            temphist.append('{} * {} = {}'.format(array[a], array[b], array[a] * array[b]))
+            if array[a] * array[b] == target:
+                    print(temphist)
+            temparr = array.copy()
+            temparr[a] *= temparr[b]
+            temparr.remove(temparr[b])
+            tempdf = pd.DataFrame({'Numbers': [temparr], 'History': [temphist]})
+            arr_of_dfs.append(tempdf)
+    
+    return pd.concat(arr_of_dfs)
+
+def multiplier2(array, history, df, target):
     l = len(array)
     for a in range(l):
         for b in np.arange(a + 1, l):
@@ -69,15 +156,32 @@ def multiplier(array, history, df, target):
             temparr = np.copy(array)
             temparr[a] *= temparr[b]
             np.delete(temparr, b)
-            '''
-            temp = pd.DataFrame({'Numbers': [], 'History': []})
-            temp = temp.append({'Numbers': temparr, 'History': temphist}, ignore_index = True)
-            '''
             df = df.append({'Numbers': temparr, 'History': temphist}, ignore_index = True)
     return df
 
 def minus(array, history, df, target):
-    array = np.sort(array)
+    array.sort(reverse = True)
+    l = len(array)
+    temparr = []
+    arr_of_dfs = []
+    arr_of_dfs.append(df)
+    for a in range(l):
+        for b in np.arange(a + 1, l):
+            if array[a] > array[b]:
+                temphist = history.copy()
+                temphist.append('{} - {} = {}'.format(array[a], array[b], array[a] - array[b]))
+                if array[a] - array[b] == target:
+                        print(temphist)
+                temparr = array.copy()
+                temparr[a] -= temparr[b]
+                temparr.remove(temparr[b])
+                tempdf = pd.DataFrame({'Numbers': [temparr], 'History': [temphist]})
+                arr_of_dfs.append(tempdf)
+    
+    return pd.concat(arr_of_dfs)
+
+def minus2(array, history, df, target):
+    array.sort(reverse = True)
     l = len(array)
     for a in range(l):
         for b in np.arange(a + 1, l):
@@ -91,20 +195,31 @@ def minus(array, history, df, target):
                 temparr[a] -= temparr[b]
                 np.delete(temparr, b)
                 df = df.append({'Numbers': temparr, 'History': temphist}, ignore_index = True)
-            '''  
-            elif array[b] > array[a]:
-                answer = array[b] - array[a]
-                temphist.append('{} - {} = {}'.format(array[b], array[a], answer))
-                if answer == target:
-                    print(temphist)
-                temparr[b] -= temparr[a]
-                temparr.remove(temparr[a])
-                df = df.append({'Numbers': temparr, 'History': temphist}, ignore_index = True)
-            '''
     return df
 
 def division(array, history, df, target):
-    array = np.sort(array)
+    array.sort(reverse = True)
+    l = len(array)
+    temparr = []
+    arr_of_dfs = []
+    arr_of_dfs.append(df)
+    for a in range(l):
+        for b in np.arange(a + 1, l):
+            if array[a] % array[b] == 0:
+                temphist = history.copy()
+                temphist.append('{} / {} = {}'.format(array[a], array[b], array[a] / array[b]))
+                if array[a] / array[b] == target:
+                        print(temphist)
+                temparr = array.copy()
+                temparr[a] /= temparr[b]
+                temparr.remove(temparr[b])
+                tempdf = pd.DataFrame({'Numbers': [temparr], 'History': [temphist]})
+                arr_of_dfs.append(tempdf)
+    
+    return pd.concat(arr_of_dfs)
+
+def division2(array, history, df, target):
+    array = np.flipud(np.sort(array))
     l = len(array)
     for a in range(l):
         for b in np.arange(a + 1, l):
@@ -118,33 +233,4 @@ def division(array, history, df, target):
                 temparr[a] /= temparr[b]
                 np.delete(temparr, b)
                 df = df.append({'Numbers': temparr, 'History': temphist}, ignore_index = True)
-            '''
-            elif array[b] % array[a] == 0:
-                answer = array[b] / array[a]
-                temphist.append('{} / {} = {}'.format(array[b], array[a], answer))
-                if answer == target:
-                    print(temphist)
-                temparr[b] /= temparr[a]
-                temparr.remove(temparr[a])
-                df = df.append({'Numbers': temparr, 'History': temphist}, ignore_index = True)
-            '''
     return df
-'''                        
-df = pd.DataFrame({'Numbers': [], 'History': []})
-numbers = [25, 5, 4, 3, 3, 8]
-target = 921
-df = df.append({'Numbers': np.sort(numbers), 'History': []}, ignore_index = True)
-processing = True
-i = 0
-while processing:
-    try:
-        df.iloc[i]
-    except:
-        processing = False
-        break
-    df = adder(df['Numbers'].iloc[i], df['History'].iloc[i], df, target)
-    df = multiplier(df['Numbers'].iloc[i], df['History'].iloc[i], df, target)
-    df = minus(df['Numbers'].iloc[i], df['History'].iloc[i], df, target)
-    df = division(df['Numbers'].iloc[i], df['History'].iloc[i], df, target)
-    i += 1
-'''
